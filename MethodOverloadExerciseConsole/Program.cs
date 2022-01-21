c9898645abfc667138e3ce271348fed2455e2031
Adam Brannon <adam1320@rocketmail.com>
2022-01-21 08:59:10 -0500

    initial commit

---
+++ MethodOverloadExerciseConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodOverloadExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isDollar)
+        {
+            var sum = num1 + num2;
+
+            if (isDollar == true && sum == 1) 
+            {
+                return ($"{sum} dollar");
+            }
+            else if (isDollar == true && sum > 1)    
+            {
+                return ($"{sum} dollars");
+            }
+            else
+            {
+                return ($"{sum}");
+            }
+
+        }
+    }
+}
